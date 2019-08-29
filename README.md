c4517b88ff5c0f9677676aa8eacd96eb8596358f
Dmytro Kozhukhar <nexot@users.noreply.github.com>
2019-08-29 23:57:36 +0300

    tt

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ dkozhukhar.github.io
 
 My Web Playground
 
-[Kontra Game Engine Playground](./kontra/)
+[Tiled Map Editor Playground](./tiled/)
+
+[Kontra Game Engine Playground - Asteroids](./kontra/)
 
 [Mapbox Playground](./mapbox/)
---
+++ tiled/fantasy-tileset.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="fantasy-tileset" tilewidth="32" tileheight="32" tilecount="208" columns="8">
+ <image source="fantasy-tileset.png" width="256" height="832"/>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="2" x="6" y="18" width="1"/>
+   <object id="3" x="17" y="18"/>
+  </objectgroup>
+ </tile>
+</tileset>
